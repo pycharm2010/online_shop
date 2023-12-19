30c794d682574eb1ae3d0904ad54ba5f7d096ecf
pythonprodev1 <149892585+pythonprodev1@users.noreply.github.com>
2023-12-19 12:51:46 +0500

    Posts qismiga modellar qo'shildi admin post qilishi uchun

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from posts.models import Post, Category, SubCategory, PostComment, PostLike, Image
+
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(PostComment)
+admin.site.register(PostLike)
+admin.site.register(Image)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,63 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
+from django.db.models import UniqueConstraint
+
+from shred.models import BaseModel
+
 
 # Create your models here.
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=256, )
+    image = models.ImageField(upload_to='category_image/', validators=[
+        FileExtensionValidator(allowed_extensions=['jpeg', 'jpg', 'png'])])
+
+    ads_count = models.IntegerField(default=0)
+
+
+class SubCategory(BaseModel):
+    title = models.CharField(max_length=256)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='categores')
+
+    ads_count = models.IntegerField(default=0)
+
+
+class Post(BaseModel):
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=256)
+    price = models.IntegerField(default=0)
+    contact = models.TextField()
+    is_top = models.BooleanField(default=False)
+
+
+class Image(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_image/')
+
+
+class PostComment(BaseModel):
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='child',
+        null=True,
+        blank=True
+    )
+
+
+class PostLike(BaseModel):
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['author', 'post'],
+                name='postLikeUnique'
+            )
+        ]
