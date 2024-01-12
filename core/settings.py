e2e9d7dff36c010ab488856aa09b6e54b47f92ec
pythonprodev1 <149892585+pythonprodev1@users.noreply.github.com>
2024-01-12 10:06:14 +0500

    stat

--- core/settings.py
+++ core/settings.py
@@ -170,10 +170,10 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'root/online_shop/staticfiles/'
+STATIC_URL = 'static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "media-files"
